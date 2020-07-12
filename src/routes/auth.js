9f8af44164405c2c489f0f5857cfa72028242893
Joe Shen <joe_shen@sfu.ca>
2020-07-12 16:47:34 -0700

    add express-validator

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,18 @@ const router = express.Router();
 import AuthController from '../controllers/AuthController';
 const controller = new AuthController();
 
-router.post("/login", (req, res) => controller.login(req, res));
+import { body, validationResult } from'express-validator';
+
+router.post("/login", [
+    body('username').trim().isAscii(),
+    body('password').trim().isAscii(),
+    body('*').escape()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    controller.login(req, res)
+});
 
 export default router
